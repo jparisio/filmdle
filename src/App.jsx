8598aeefda703efc6d40f6cc0b27ee051af19241
Justin Parisio <justinparisio@Justins-M3-MacBook-Pro.local>
2024-04-04 16:16:43 -0400

    search changes

--- src/App.jsx
+++ src/App.jsx
@@ -73,10 +73,6 @@ export default function App() {
   }
 
   function formatTime(seconds) {
-    if (seconds <= 0) {
-      currentIndex++;
-      resetLocalStorage();
-    }
     const hours = Math.floor(seconds / 3600);
     const minutes = Math.floor((seconds % 3600) / 60);
     const remainingSeconds = seconds % 60;
--- src/components/search bar/SearchBar.jsx
+++ src/components/search bar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import "./SearchBar.css";
 
 export default function SearchBar({ handleSearch, handleGuess, data, search }) {
@@ -7,36 +7,44 @@ export default function SearchBar({ handleSearch, handleGuess, data, search }) {
     handleSearch({ target: { value: text } });
   }
   return (
-    <div className="search-container">
-      <div className="dropdown">
-        <input
-          type="text"
-          placeholder="Enter movie guess..."
-          onChange={handleSearch}
-          value={search}
-        />
-        {/* Render the dropdown content if there is data */}
-        {data.length > 0 && search != "" && (
-          <div className="dropdown-content">
-            {data.map((movie) => (
-              <p
-                key={movie.id}
-                onClick={() => handleTitleClick(movie.titleNameText)}
-              >
-                {movie.titleNameText}
-              </p>
-            ))}
-          </div>
-        )}
+    <>
+      <div className="search-container">
+        <div className="dropdown">
+          <input
+            type="text"
+            placeholder="Enter movie guess..."
+            onChange={handleSearch}
+            value={search}
+          />
+        </div>
+        <motion.button
+          onClick={() => handleGuess(search)}
+          whileTap={{
+            scale: 0.8,
+          }}
+        >
+          enter
+        </motion.button>
       </div>
-      <motion.button
-        onClick={() => handleGuess(search)}
-        whileTap={{
-          scale: 0.8,
-        }}
-      >
-        enter
-      </motion.button>
-    </div>
+      <AnimatePresence>
+        {data.length > 0 && search != "" && (
+          <motion.div className="suggestion-buttons">
+            <AnimatePresence>
+              {data.slice(0, 5).map((movie, index) => (
+                <motion.button
+                  key={movie.id}
+                  onClick={() => handleTitleClick(movie.titleNameText)}
+                  initial={{ opacity: 0, y: 50 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ delay: index * 0.1 }}
+                >
+                  {movie.titleNameText}
+                </motion.button>
+              ))}
+            </AnimatePresence>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </>
   );
 }
